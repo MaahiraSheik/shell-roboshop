02db3acfa24db6a5ceca00fd74471aeb329e430d
maahirasheik <maahisheik612@gamil.com>
2025-11-17 17:31:59 +0530

    roboshop created

--- roboshop.sh
+++ roboshop.sh
@@ -26,4 +26,5 @@ INSTANCE_IP=$(aws ec2 describe-instances \
     --instance-ids $INSTANCE_ID \
     --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
+    echo "$instance ip address: $INSTANCE_IP"
 done 
