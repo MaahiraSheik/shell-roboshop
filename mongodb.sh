2f8c2e2d571ebe63b293a56354cbcd8694c975d5
maahirasheik <maahisheik612@gamil.com>
2025-12-12 00:04:18 +0530

    shell roboshop

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+echo -e "$R ERROR: please run with this root access $N" | tee -a $LOG_FILE
+exit 1
+else
+echo "your is running with root access"  | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]; then
+    echo -e "$G SUCCESS: $2 installed $N"  | tee -a $LOG_FILE
+  else
+    echo -e "$R FAILURE: $2 not installed $N"  | tee -a $LOG_FILE
+    exit 1
+  fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "install mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enable Mongodb service"
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "strat Mongodb service"
+
+sed -i -e 's/127.0.0.1 to 0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing mongodb conf file for remote connections"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "restart mongodb"
