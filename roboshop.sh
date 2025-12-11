075fc0cc7f07b7aaa0249b5dd13814590c6c3a62
maahirasheik <maahisheik612@gamil.com>
2025-12-11 17:08:09 +0530

    shell roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -59,21 +59,20 @@ INSTANCE_IP=$(aws ec2 describe-instances \
     echo "$instance ip address: $INSTANCE_IP"
 
     aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-    "Comment": "Creating a record set for cognito endpoint"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$instance'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP'"
+  --hosted-zone-id "$ZONE_ID" \
+  --change-batch '{
+    "Comment": "Creating a record set for cognito endpoint",
+    "Changes": [{
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'"$instance.$DOMAIN_NAME"'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [{
+          "Value": "'"$IP"'"
         }]
       }
     }]
-  }
+  }'
 
 done 
